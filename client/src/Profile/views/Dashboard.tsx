11d81e9ca6617eee39d4b0b4921bc3a7a4941a22
pankajchangra <pankajchangra788@gmail.com>
2020-04-15 17:04:53 +0530

    changes for admin panel

--- client/src/Profile/views/Dashboard.tsx
+++ client/src/Profile/views/Dashboard.tsx
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import { Card } from "../Card/Card";
-import { Tasks } from "../Tasks/Tasks";
-import TableList from "./TableList"
+import { Tasks } from "../Tasks/Tasks"
+import ChartistGraph from "react-chartist";
+
+import {
+  dataPie,
+  legendPie,
+  dataSales,
+  optionsSales,
+  responsiveSales,
+  legendSales,
+} from "../variables/Variables";
 
 class Dashboard extends Component {
   createLegend(json?: any) {
@@ -18,9 +27,51 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="content">
-        <TableList />
         <div className="fluid">
           <Row>
+          <Col md={8}>
+              <Card
+                statsIcon="fa fa-history"
+                // id="chartHours"
+                title="Users Behavior"
+                category="24 Hours performance"
+                stats="Updated 3 minutes ago"
+                content={
+                  <div className="ct-chart">
+                    <ChartistGraph
+                      data={dataSales}
+                      type="Line"
+                      options={optionsSales}
+                      responsiveOptions={responsiveSales}
+                    />
+                  </div>
+                }
+                legend={
+                  <div className="legend">{this.createLegend(legendSales)}</div>
+                }
+              />
+            </Col>
+            <Col md={4}>
+              <Card
+                statsIcon="fa fa-clock-o"
+                title="Email Statistics"
+                category="Last Campaign Performance"
+                stats="Campaign sent 2 days ago"
+                content={
+                  <div
+                    id="chartPreferences"
+                    className="ct-chart ct-perfect-fourth"
+                  >
+                    <ChartistGraph data={dataPie} type="Pie" />
+                  </div>
+                }
+                legend={
+                  <div className="legend">{this.createLegend(legendPie)}</div>
+                }
+              />
+            </Col>
+            </Row>
+            <Row>
             <Col md={6}>
               <Card
                 title="Tasks"
--- client/src/Profile/views/UserProfile.tsx
+++ client/src/Profile/views/UserProfile.tsx
@@ -1,100 +1,11 @@
 import React, { Component } from "react";
-import {
-  Row,
-  Col
-} from "react-bootstrap";
-
-import { Card } from "../Card/Card";
-import { FormInputs } from "../FormInputs/FormInputs";
-import Usercards from "../UserCard/UserCard";
-import Button from "../CustomButton/CustomButton";
-import avatar from "../../assets/img/faces/face-3.jpg";
+import TableList from "./TableList";
 
 class UserProfile extends Component {
   render() {
     return (
       <div className="content">
-        <div className="fluid">
-          <Row>
-            <Col md={8}>
-              <Card
-                title="Edit Profile"
-                content={
-                  <form>
-                    <FormInputs
-                      ncols={["col-md-5", "col-md-3", "col-md-4"]}
-                      properties={[
-                        {
-                          label: "Company (disabled)",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "Company",
-                          defaultValue: "Creative Code Inc.",
-                          disabled: true
-                        },
-                        {
-                          label: "Email address",
-                          type: "email",
-                          bsClass: "form-control",
-                          placeholder: "Email"
-                        }
-                      ]}
-                    />
-                    <FormInputs
-                      ncols={["col-md-6", "col-md-6"]}
-                      properties={[
-                        {
-                          label: "First name",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "First name",
-                          defaultValue: "Mike"
-                        },
-                        {
-                          label: "Last name",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "Last name",
-                          defaultValue: "Andrew"
-                        }
-                      ]}
-                    />                   
-                    <Button bsStyle="info" pullRight fill type="submit">
-                      Update Profile
-                    </Button>
-                    <div className="clearfix" />
-                  </form>
-                }
-              />
-            </Col>
-            <Col md={4}>
-              <Usercards
-                bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
-                avatar={avatar}
-                name="Mike Andrew"
-                userName="michael24"
-                description={
-                  <span>
-                    Head of the company
-                  </span>
-                }
-                socials={
-                  <div>
-                    <Button simple>
-                      <i className="fa fa-facebook-square" />
-                    </Button>
-                    <Button simple>
-                      <i className="fa fa-twitter" />
-                    </Button>
-                    <Button simple>
-                      <i className="fa fa-google-plus-square" />
-                    </Button>
-                  </div>
-                }
-              />
-            </Col>
-          </Row>
-        </div>
+        <TableList />
       </div>
     );
   }
